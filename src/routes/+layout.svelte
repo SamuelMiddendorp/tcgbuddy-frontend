6d1f1a2d3a95fbac29f850fb3a564de0c450e9df
Samuel Middendorp <samiezaurus@gmail.com>
2023-03-21 23:17:02 +0100

    Add initial layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,23 @@
 <svelte:head>
 	<title>TCGbuddy</title>
 </svelte:head>
+<style>
+   .layout-container{
+        display: grid;
+        grid-template-columns: max-content 1fr;
+        grid-template-rows: max-content 1fr;
+   } 
+</style>
 <body>
+    <div class="layout-container">
+        <div class="header">
 
+        </div>
+        <div class="function-navigator">
+
+        </div>
+        <div class="main-content">
+            <slot/>
+        </div>
+    </div>
 </body>
