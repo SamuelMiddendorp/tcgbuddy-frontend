c5f98688acf1791a771717fcdb7733163537a10c
Samuel Middendorp <samiezaurus@gmail.com>
2023-03-22 00:22:49 +0100

    Some transitions on click for the function-selector

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
 import Logo from '../lib/components/Logo.svelte';
 import './layout.css';
+    let panelOpen: boolean = true;
+
+    function togglePanel(){
+        const elem = document.querySelector<HTMLElement>('.function-navigator')!;
+        if(panelOpen){
+            elem.style.maxWidth = "2em";
+            panelOpen = !panelOpen;
+            return;
+        }
+        elem.style.maxWidth = "6em";
+        panelOpen = !panelOpen;
+    }
 </script>
 
 <svelte:head>
@@ -8,7 +20,7 @@ import './layout.css';
 </svelte:head>
 <body>
     <div class="layout-container">
-        <div class="header">
+        <div class="header panel">
             <div class="logo">
                 <Logo/>
             </div>
@@ -18,12 +30,12 @@ import './layout.css';
                 <a href="/">Logout</a>
             </div>
         </div>
-        <div class="function-navigator">
+        <div class="function-navigator panel" on:click={() => togglePanel()}>
                 <a href="/">Cards</a>
                 <a href="/">Decks</a>
                 <a href="/">News</a>
         </div>
-        <div class="main-content">
+        <div class="main-content panel">
             <slot />
         </div>
     </div>
