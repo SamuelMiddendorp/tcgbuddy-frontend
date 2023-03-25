73092e3fa012b7fbe0b2893401979dc0b76fc407
Samuel Middendorp <samiezaurus@gmail.com>
2023-03-25 23:56:18 +0100

    Add some sort of mobile friendlyness

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-import Logo from '../lib/components/Logo.svelte';
-import './layout.css';
+    import Logo from "../lib/components/Logo.svelte";
+    import "./layout.css";
     let panelOpen: boolean = true;
 
-    function togglePanel(){
-        const elem = document.querySelector<HTMLElement>('.function-navigator')!;
-        if(panelOpen){
+    function togglePanel() {
+        const elem = document.querySelector<HTMLElement>(
+            ".function-navigator"
+        )!;
+        if (panelOpen) {
             elem.style.maxWidth = "2em";
             panelOpen = !panelOpen;
             return;
@@ -22,7 +24,7 @@ import './layout.css';
     <div class="layout-container">
         <div class="header panel">
             <div class="logo">
-                <Logo/>
+                <Logo />
             </div>
             <div class="session-controls">
                 <a href="/">Profile</a>
@@ -31,16 +33,164 @@ import './layout.css';
             </div>
         </div>
         <div class="function-navigator panel" on:click={() => togglePanel()}>
-                <a href="/">CARDS</a>
-                <a href="/">DECKS</a>
-                <a href="/">WISHLISTS</a>
-                <a href="/">LGS CORNER</a>
-                <a href="/">NEWS</a>
-                <a href="/">CARD OF THE DAY</a>
+            <a href="/"
+                ><span>CARDS</span>
+                <svg
+                    width="10.452028mm"
+                    height="11.600512mm"
+                    version="1.1"
+                    viewBox="0 0 10.452028 11.600512"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:cc="http://creativecommons.org/ns#"
+                    xmlns:dc="http://purl.org/dc/elements/1.1/"
+                    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                >
+                    <g
+                        transform="translate(-91.962555,-151.91574)"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-width="1.1"
+                    >
+                        <path
+                            class="icon-1"
+                            stroke="#fff"
+                            d="m95.249999 153.45833h6.614581v9.26042h-6.614581z"
+                        />
+                        <path
+                            class="icon-2"
+                            stroke="#fff"
+                            d="m92.60417 153.77158 6.50218-1.21422 1.6999 9.10306-6.502181 1.21422z"
+                        />
+                    </g>
+                </svg>
+            </a>
+            <a href="/"
+                ><span>CARDS</span>
+                <svg
+                    width="10.452028mm"
+                    height="11.600512mm"
+                    version="1.1"
+                    viewBox="0 0 10.452028 11.600512"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:cc="http://creativecommons.org/ns#"
+                    xmlns:dc="http://purl.org/dc/elements/1.1/"
+                    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                >
+                    <g
+                        transform="translate(-91.962555,-151.91574)"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-width="1.1"
+                    >
+                        <path
+                            class="icon-1"
+                            stroke="#fff"
+                            d="m95.249999 153.45833h6.614581v9.26042h-6.614581z"
+                        />
+                        <path
+                            class="icon-2"
+                            stroke="#fff"
+                            d="m92.60417 153.77158 6.50218-1.21422 1.6999 9.10306-6.502181 1.21422z"
+                        />
+                    </g>
+                </svg>
+            </a>
+            <a href="/"
+                ><span>CARDS</span>
+                <svg
+                    width="10.452028mm"
+                    height="11.600512mm"
+                    version="1.1"
+                    viewBox="0 0 10.452028 11.600512"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:cc="http://creativecommons.org/ns#"
+                    xmlns:dc="http://purl.org/dc/elements/1.1/"
+                    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                >
+                    <g
+                        transform="translate(-91.962555,-151.91574)"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-width="1.1"
+                    >
+                        <path
+                            class="icon-1"
+                            stroke="#fff"
+                            d="m95.249999 153.45833h6.614581v9.26042h-6.614581z"
+                        />
+                        <path
+                            class="icon-2"
+                            stroke="#fff"
+                            d="m92.60417 153.77158 6.50218-1.21422 1.6999 9.10306-6.502181 1.21422z"
+                        />
+                    </g>
+                </svg>
+            </a>
+            <a href="/"
+                ><span>CARDS</span>
+                <svg
+                    width="10.452028mm"
+                    height="11.600512mm"
+                    version="1.1"
+                    viewBox="0 0 10.452028 11.600512"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:cc="http://creativecommons.org/ns#"
+                    xmlns:dc="http://purl.org/dc/elements/1.1/"
+                    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                >
+                    <g
+                        transform="translate(-91.962555,-151.91574)"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-width="1.1"
+                    >
+                        <path
+                            class="icon-1"
+                            stroke="#fff"
+                            d="m95.249999 153.45833h6.614581v9.26042h-6.614581z"
+                        />
+                        <path
+                            class="icon-2"
+                            stroke="#fff"
+                            d="m92.60417 153.77158 6.50218-1.21422 1.6999 9.10306-6.502181 1.21422z"
+                        />
+                    </g>
+                </svg>
+            </a>
+            <a href="/"
+                ><span>CARDS</span>
+                <svg
+                    width="10.452028mm"
+                    height="11.600512mm"
+                    version="1.1"
+                    viewBox="0 0 10.452028 11.600512"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:cc="http://creativecommons.org/ns#"
+                    xmlns:dc="http://purl.org/dc/elements/1.1/"
+                    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                >
+                    <g
+                        transform="translate(-91.962555,-151.91574)"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-width="1.1"
+                    >
+                        <path
+                            class="icon-1"
+                            stroke="#fff"
+                            d="m95.249999 153.45833h6.614581v9.26042h-6.614581z"
+                        />
+                        <path
+                            class="icon-2"
+                            stroke="#fff"
+                            d="m92.60417 153.77158 6.50218-1.21422 1.6999 9.10306-6.502181 1.21422z"
+                        />
+                    </g>
+                </svg>
+            </a>
         </div>
         <div class="main-content panel">
             <slot />
         </div>
     </div>
 </body>
-
