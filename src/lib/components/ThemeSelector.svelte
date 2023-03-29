e8f8fa4621c3b0946a8b2c778f82e9780b0061a7
Samuel Middendorp <samiezaurus@gmail.com>
2023-03-29 22:32:49 +0200

    Add theme selector functionality

--- src/lib/components/ThemeSelector.svelte
+++ src/lib/components/ThemeSelector.svelte
@@ -1,21 +1,35 @@
 <script lang="ts">
+    const setTheme = (lightTheme: boolean) => {
+        let background = "#111";
+        let textColor = "#efefef";
+        let panelColor = "#222";
+        if(lightTheme){ 
+            background = "#efefef";
+            textColor = "#111";
+            panelColor = "#eaeaea";
+        }
+        document.documentElement.style.setProperty('--background', background);
+        document.documentElement.style.setProperty('--main-link-color', textColor);
+        document.documentElement.style.setProperty('--outer-panel-color', panelColor);
 
+    }
 </script>
 <style>
     div{
         position: fixed;
         top: 0;
         left: 0;
-        background-color: #ff0000;
     }
     .light-selector{
         display: block;
-        background-color: #efefef;
+        background-color: #efefcf;
         width: 2em;
         height: 2em;
+        border-top-right-radius: 100vw;
     }
     .dark-selector{
         display: block;
+        border-bottom-right-radius: 100vw;
         width: 2em;
         height: 2em;
         background-color: #111;
@@ -23,7 +37,7 @@
 </style> 
 <body>
     <div>
-    <span class="light-selector"></span>
-    <span class="dark-selector"></span>
+    <span on:click={() => setTheme(true)} class="light-selector"></span>
+    <span on:click={() => setTheme(false)} class="dark-selector"></span>
     </div>
 </body>
