c092b5fa5b7ae1bc7a3fd98afc0932b94c9d71ea
Samuel Middendorp <samiezaurus@gmail.com>
2023-04-01 15:55:01 +0200

    Add alt to pokeimage

--- src/routes/card/[cardId]/+page.svelte
+++ src/routes/card/[cardId]/+page.svelte
@@ -7,5 +7,5 @@
 <body>
     <h2>Card name: {data.card.name}</h2>
     <h2>Card hp: {data.card.hp}</h2>
-    <img src="{data.card.images.small}"/>
+    <img src="{data.card.images.small}" alt="cute pokemon"/>
 </body>
