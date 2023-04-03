1a5b5dd09db2658c2bd2abb28d3770dd7d935628
Samuel Middendorp <samiezaurus@gmail.com>
2023-04-03 22:17:45 +0200

    Update decks page

--- src/routes/deck/+page.svelte
+++ src/routes/deck/+page.svelte
@@ -1,1 +1,15 @@
-<h1>Deck</h1>
+<h1>Decks:</h1>
+<div class="decks-container">
+{#each Array(5) as _, index (index)}
+<div>
+    <h2>Deck{index}</h2>    
+    </div>
+{/each}
+</div>
+<style>
+    .decks-container{
+        margin-top: 1em;
+        display: grid;
+        grid-auto-flow: column;
+    }
+</style>
