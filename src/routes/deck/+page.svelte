c13a6a7b231c88c8e466b289c3530c11db973ca8
Samuel Middendorp <samiezaurus@gmail.com>
2023-04-03 22:40:01 +0200

    Add links to arbitrairy decks

--- src/routes/deck/+page.svelte
+++ src/routes/deck/+page.svelte
@@ -1,15 +1,29 @@
 <h1>Decks:</h1>
 <div class="decks-container">
 {#each Array(5) as _, index (index)}
-<div>
-    <h2>Deck{index}</h2>    
-    </div>
+<a class="deck-link" href="deck/{index}">
+    <h2>Deck{index}</h2>
+    </a>    
 {/each}
 </div>
 <style>
     .decks-container{
         margin-top: 1em;
         display: grid;
+        grid-auto-columns: minmax(20em, max-content);
         grid-auto-flow: column;
+        grid-auto-rows: 20em;
+        justify-content: start;
+    }
+    .deck-link{
+        transition: all 0.2s ease-out;
+        padding: 2em;
+        margin-right: 1em;
+        border-radius: var(--border-radius);
+        background-color: var(--inner-panel-color);
+    }
+    .deck-link:hover{
+        border-radius: 2em;
+        background-color: var(--statement-color);
     }
 </style>
