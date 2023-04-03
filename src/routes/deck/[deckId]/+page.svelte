74751179bfa6c16f438c5478f225572caa5a64fc
Samuel Middendorp <samiezaurus@gmail.com>
2023-04-03 21:42:34 +0200

    Add stats for a deck

---
+++ src/routes/deck/[deckId]/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    export let data: any;
+</script>
+
+<div class="deck-container">
+    <div class="deck-descriptor">
+        <h1>{data.deck.name}</h1>
+        <p>{data.deck.description}</p>
+    </div>
+    {#each data.deck.stats as stat}
+    <div class="deck-descriptor">
+        <h2>{stat.type}</h2>
+        <p>{stat.card}</p>
+    </div>
+    {/each}
+</div>
+
+<style>
+    .deck-container {
+        display: grid;
+        grid-auto-columns: max-content;
+        grid-auto-flow: column;
+        grid-auto-rows: minmax(15em, max-content);
+    }
+    .deck-container h1 {
+        margin-bottom: 1em;
+    }
+    .deck-descriptor h2 {
+        margin-bottom: 1em;
+    }
+    .deck-descriptor{
+        justify-self: center;
+        margin-right: 1em;
+        padding: 1em;
+        background-color: var(--inner-panel-color);
+        border-radius: var(--border-radius);
+    }
+</style>
---
+++ src/routes/deck/[deckId]/+page.ts
@@ -0,0 +1,22 @@
+export const load = ({params}) => {
+    return{
+        deck: {
+            "name": `deck${params.deckId}`,
+            "description": "A nice deck to play that is not hard to learn",
+            "stats": [
+                {
+                    "type": "mainAttacker",
+                    "card": "Zapdos"
+                },
+                {
+                    "type": "benchSupport",
+                    "card": "Manaphy"
+                },
+                {
+                    "type": "synergySupporter",
+                    "card": "Sonia"
+                }
+        ]
+        }
+    }
+}
