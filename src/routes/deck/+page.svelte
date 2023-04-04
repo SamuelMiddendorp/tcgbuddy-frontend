ebffdb74cc7293b9a8598e647ef3d7e2b04ff839
Samuel Middendorp <samiezaurus@gmail.com>
2023-04-04 21:17:41 +0200

    Fix bug where container is always 100vh and never more

--- src/routes/deck/+page.svelte
+++ src/routes/deck/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Decks:</h1>
 <div class="decks-container">
-{#each Array(5) as _, index (index)}
+{#each Array(20) as _, index (index)}
 <a class="deck-link" href="deck/{index}">
     <h2>Deck{index}</h2>
     </a>    
