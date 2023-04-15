aec6d622be431579c486346548bb132f480da94d
Samuel <samiezaurus@gmail.com>
2023-04-15 21:28:20 +0200

    Add pie charts to distirbution components

--- src/lib/components/CardDistribution.svelte
+++ src/lib/components/CardDistribution.svelte
@@ -2,16 +2,39 @@
    export let deck;
    export let cardType: string;
    let percentage = 0;
-   $:{
-     let totalCards = 0;
-     let relevantCards = 0;
-     deck.cardList.forEach(card => {
-      totalCards += card.count
-      if(card.type == cardType){
-         relevantCards += card.count
-      }
-     }); 
-     percentage = Math.round(relevantCards / totalCards * 100) 
+   $: {
+      let totalCards = 0;
+      let relevantCards = 0;
+      deck.cardList.forEach((card) => {
+         totalCards += card.count;
+         if (card.type == cardType) {
+            relevantCards += card.count;
+         }
+      });
+      percentage = Math.round((relevantCards / totalCards) * 100);
    }
 </script>
-<div class="cardAmountPie">{percentage}</div>
+
+<div class="cardAmountPie" style="--p:{percentage}">{percentage}%</div>
+
+<style>
+   .cardAmountPie {
+      position: relative;
+      width: 50%;
+      aspect-ratio: 1;
+      display: inline-grid;
+      place-content: center;
+      margin: 1rem;
+      z-index: 1;
+      font-size: 1rem;
+   }
+   .cardAmountPie:before {
+      content: "";
+      position: absolute;
+      border-radius: 50%;
+      inset: 0;
+      z-index: -1;
+      border: 1px solid var(--statement-color);
+      background: conic-gradient(var(--statement-color) calc(var(--p) * 1%), #0000 0);
+   }
+</style>
--- src/routes/deck/[deckId]/+page.svelte
+++ src/routes/deck/[deckId]/+page.svelte
@@ -23,12 +23,15 @@
             </div>
         {/each}
         <div class="deck-descriptor">
+            <h2 style="float: right">Trainers</h2>
             <CardDistribution cardType={"Trainer"} deck={data.deck}/>
         </div>
         <div class="deck-descriptor">
+            <h2 style="float: right">Pokemon</h2>
             <CardDistribution cardType={"Pokemon"} deck={data.deck}/>
         </div>
         <div class="deck-descriptor">
+            <h2 style="float: right">Energy</h2>
             <CardDistribution cardType={"Energy"} deck={data.deck}/>
         </div>
     </div>
