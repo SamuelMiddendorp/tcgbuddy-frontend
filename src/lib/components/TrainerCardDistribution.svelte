c6bdb58090c6c6283de9705cfcea5a6174a46ba2
Samuel <samiezaurus@gmail.com>
2023-04-15 20:58:33 +0200

    Add TrainerCardDistribution component

---
+++ src/lib/components/TrainerCardDistribution.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+   export let deck;
+   let percentage = 0;
+   $:{
+     let totalCards = 0;
+     let relevantCards = 0;
+     deck.cardList.forEach(card => {
+      totalCards += card.count
+      if(card.type == "Trainer"){
+         relevantCards += card.count
+      }
+     }); 
+     percentage = Math.round(relevantCards / totalCards * 100) 
+   }
+</script>
+<div class="cardAmountPie">{percentage}</div>
